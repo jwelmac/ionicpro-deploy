742528d887e89ff3f0c11e82019bde96bd7b9739
Jermaine McFarlane <j.mcfarlane.mbbs@gmail.com>
2017-12-13 13:25:33 -0400

    chore: improve decorator warning

--- src/ionic-pro-deploy.decorators.ts
+++ src/ionic-pro-deploy.decorators.ts
@@ -1,6 +1,9 @@
 import { Observable } from 'rxjs/Observable';
 
-
+/**
+ * Check if the Ionic Pro Plugin is installed before running a dependent function
+ * When deploy is not available failure method for return type (Promise or Observable triggered)
+ */
 export function checkDeploy(isObservable: boolean = false) {
   return function checkDeployfactory(target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) {
     const originalMethod = descriptor.value;
@@ -9,8 +12,9 @@ export function checkDeploy(isObservable: boolean = false) {
         if (this.deploy) {
           return originalMethod.apply(this, args);
         } else {
-          const error = 'IonicCordova is not available.';
-          console.warn(`${error} Ensure cordova-plugin-ionic is installed`);
+          const error = 'IonicCordova is not available';
+          // tslint:disable-next-line:max-line-length
+          console.warn(`Tried calling IonicProDeployService.${propertyKey} but ${error}. Ensure cordova-plugin-ionic is installed or cordova is available.`);
           return isObservable ? Observable.create(obs => obs.error(error)) : Promise.reject(error);
         }
     };
